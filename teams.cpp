5b0eacd11fc62edb30b9616cc995a491aa31f0b7
Szymon Łukasik <sl428760@students.mimuw.edu.pl>
2022-02-11 04:03:23 +0100

    Initial commit

---
+++ teams.cpp
@@ -0,0 +1,49 @@
+#include <utility>
+#include <deque>
+#include <future>
+
+#include "teams.hpp"
+#include "contest.hpp"
+#include "collatz.hpp"
+
+ContestResult TeamNewThreads::runContestImpl(ContestInput const & contestInput)
+{
+    ContestResult r;
+    //TODO
+    return r;
+}
+
+ContestResult TeamConstThreads::runContestImpl(ContestInput const & contestInput)
+{
+    ContestResult r;
+    //TODO
+    return r;
+}
+
+ContestResult TeamPool::runContest(ContestInput const & contestInput)
+{
+    ContestResult r;
+    //TODO
+    return r;
+}
+
+ContestResult TeamNewProcesses::runContest(ContestInput const & contestInput)
+{
+    ContestResult r;
+    //TODO
+    return r;
+}
+
+ContestResult TeamConstProcesses::runContest(ContestInput const & contestInput)
+{
+    ContestResult r;
+    //TODO
+    return r;
+}
+
+ContestResult TeamAsync::runContest(ContestInput const & contestInput)
+{
+    ContestResult r;
+    //TODO
+    return r;
+}
