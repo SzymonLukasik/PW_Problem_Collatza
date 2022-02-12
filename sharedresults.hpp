4aae53b9530fa49834aeed393c53955771e47ee5
Szymon Łukasik <sl428760@students.mimuw.edu.pl>
2022-02-12 10:56:14 +0100

    Little changes

--- sharedresults.hpp
+++ sharedresults.hpp
@@ -9,19 +9,24 @@
 class SharedResults
 {
 public:
+
+    SharedResults() {
+        for (uint64_t i = 0; i < size; i++)
+            cache[i] = 0;
+    }
+
     uint64_t get_result(const InfInt & input) {
-        cache_t::iterator it = this->cache.find(input);
-        if (it == this->cache.end()) {
+        if (input > size || (input != 1 && cache[input.toUnsignedLongLong()] == 0)) {
             uint64_t res = calcCollatz(input);
-            cache[input] = res;
+            cache[input.toUnsignedLongLong()] = res;
             return res;
         }
-        return it->second;
+        return cache[input.toUnsignedLongLong()];
     }
 
 private:
-    using cache_t = std::map<InfInt, uint64_t>;
-    cache_t cache;
+    static const uint64_t size = 1e6;
+    uint64_t cache [size];
 };
 
 #endif
